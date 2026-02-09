738b0bb328c678b528563d075227c6863ddf5cb0
Sabribrina <sabrinal117@nycstudents.net>
2026-02-09 10:17:17 -0500

    2/9/26

---
+++ numbergameaha.py
@@ -0,0 +1,8 @@
+guess = int(input("how many chickens would a turkey eat? ahahahahha"))
+import random
+print = (random.randint(1,10))
+while random == guess:
+    if guess == random:
+        print("how did you know?!?")
+    else:
+        print(guess)
