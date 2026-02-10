4ed1e1292c9f6b0807575c7652f9a4892e200755
Sabribrina <sabrinal117@nycstudents.net>
2026-02-10 10:08:22 -0500

    2/10/26

--- numbergameaha.py
+++ numbergameaha.py
@@ -1,8 +1,20 @@
-guess = int(input("how many chickens would a turkey eat? ahahahahha"))
+guess = int(input('guess the number of children in my basement...'))
+history = []
+history.append(guess)
+
 import random
-print = (random.randint(1,10))
-while random == guess:
-    if guess == random:
-        print("how did you know?!?")
-    else:
-        print(guess)
+secret = (random.randint(1,10))
+
+while secret is not guess:
+    for i in range (secret):
+        if secret < guess:
+            guess = int(input('guess the number of children in my basement...'))
+            history.append(i)
+            print('lower bro!! im not insane' and history)
+        if guess < secret:
+            guess = int(input('guess the number of children in my basement...'))
+            history.append(i)
+            print('higher dude' and history)
+
+if guess == secret:
+    print('thats right!!' and history and guess)
